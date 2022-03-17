621a958294f4ee2aeb76f318ced35d507dec2880
Gwendal <gwendou95@hotmail.fr>
2022-03-17 18:28:19 +0100

    WIP Fake animators

--- src/test/java/use_case/FakeAnimators.java
+++ src/test/java/use_case/FakeAnimators.java
@@ -1,7 +1,28 @@
 package use_case;
 
+import model.Animator;
 import model.Animators;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
+
 public class FakeAnimators implements Animators {
+    private HashMap<UUID, Animator> animators = new HashMap<>();
+
+    public FakeAnimators() {
+        for (int i = 0; i < 4; i++) {
+            UUID id = UUID.randomUUID();
+            animators.put(id, new Animator(id, new HashSet<>()));
+        }
+    }
+
+    @Override
+    public Animator findById(UUID id) {
+        return animators.get(id);
+    }
+
 
 }
