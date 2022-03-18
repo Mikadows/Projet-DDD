511cffb929ba4b05921a6cdcc17155e860e052b1
chukitipok <antonin.texier@gmail.com>
2022-03-18 02:04:08 +0100

    Fix errors due to renamed.

--- src/test/java/use_case/FakeSpaces.java
+++ src/test/java/use_case/FakeSpaces.java
@@ -1,34 +1,31 @@
 package use_case;
 
-import model.ScheduleRange;
+import model.Schedule;
 import model.Space;
 import model.Spaces;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class FakeSpaces implements Spaces {
-    private Set<Space> spaces;
+    private final Set<Space> spaces;
 
     public FakeSpaces() {
         spaces = new HashSet<>();
         spaces.add(new Space(5, new HashSet<>()));
-        Set<ScheduleRange> scheduleRanges = new HashSet<>();
-        scheduleRanges.add(new ScheduleRange(LocalDateTime.parse("2022-03-17T22:26:02.575492900"), Duration.ofHours(1)));
-        scheduleRanges.add(new ScheduleRange(LocalDateTime.parse("2022-03-17T19:26:02.575492900"), Duration.ofHours(1)));
-        scheduleRanges.add(new ScheduleRange(LocalDateTime.parse("2022-03-17T18:00:00.575492900"), Duration.ofHours(1)));
+        Set<Schedule> scheduleRanges = new HashSet<>();
+        scheduleRanges.add(new Schedule(LocalDateTime.parse("2022-03-17T22:26:02.575492900"), Duration.ofHours(1)));
+        scheduleRanges.add(new Schedule(LocalDateTime.parse("2022-03-17T19:26:02.575492900"), Duration.ofHours(1)));
+        scheduleRanges.add(new Schedule(LocalDateTime.parse("2022-03-17T18:00:00.575492900"), Duration.ofHours(1)));
         spaces.add(new Space(6, scheduleRanges));
     }
 
-    public void display() {
-        System.out.println(spaces);
+    public Optional<Space> findById(UUID id) {
+        return Optional.empty();
     }
 
-    @Override
-    public Space findById(UUID id) {
-        return null;
+    public void bookAvailability(Space space, Schedule Schedule) {
+
     }
 }
